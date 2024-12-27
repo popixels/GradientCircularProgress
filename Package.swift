1ea2a35a1b050fd55481a816b1e3f0ebed2b7442
Boris Bengus <boris@popixels.com>
2024-12-27 13:32:31 +0200

    TMP-1385: SPM Integration (#3)
    * TMP-1385: SPM Integration
    
    * TMP-1385: Supress warnings

---
+++ Package.swift
@@ -0,0 +1,32 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "GradientCircularProgress",
+    defaultLocalization: "en",
+    platforms: [
+        .iOS(.v15),
+    ],
+    products: [
+        .library(
+            name: "GradientCircularProgress",
+            targets: ["GradientCircularProgress"]
+        ),
+    ],
+    targets: [
+        .target(
+            name: "GradientCircularProgress",
+            path: ".",
+            sources: ["source"],
+            swiftSettings: [
+                .unsafeFlags(["-suppress-warnings"]),
+            ]
+        ),
+        .testTarget(
+            name: "GradientCircularProgressTests",
+            dependencies: ["GradientCircularProgress"]
+        ),
+    ]
+)
